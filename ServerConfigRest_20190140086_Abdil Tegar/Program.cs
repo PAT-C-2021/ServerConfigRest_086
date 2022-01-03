ce59c61c645cb5972a9ef4279e98064fd3024e44
Abdil Tegar Arifin <abdiltegar12@gmail.com>
2022-01-03 11:21:53 +0700

    first push

---
+++ ServerConfigRest_20190140086_Abdil Tegar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_20190140086_Abdil_Tegar_Arifin;
+
+namespace ServerConfigRest_20190140086_Abdil_Tegar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server is ready ...");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception e)
+            {
+                hostObjek = null;
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
